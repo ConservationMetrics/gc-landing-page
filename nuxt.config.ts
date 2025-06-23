a7069f9b6037466faf9e8197f11d39601cb7442e
conservationtimothy <ogbemu.diatimothy@gmail.com>
2025-06-23 08:47:26 +0100

    initil stuff

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
+})
