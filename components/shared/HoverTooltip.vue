c88b455672507471adf9a1c4c8f0022ba9e53736
Rudo Kemper <rtakemper@gmail.com>
2026-04-07 13:10:50 -0400

    Center GC tooltip for mobile viewport

--- components/shared/HoverTooltip.vue
+++ components/shared/HoverTooltip.vue
@@ -22,7 +22,7 @@ const tooltipId = useId();
     <span
       :id="tooltipId"
       role="tooltip"
-      class="pointer-events-none absolute left-1/2 top-full z-50 mt-2 w-max max-w-[min(34rem,calc(100vw-2rem))] -translate-x-1/2 rounded-lg bg-gray-900 px-4 py-3 text-center text-base leading-relaxed text-white shadow-xl ring-1 ring-white/10 opacity-0 transition-opacity duration-75 ease-out group-hover:opacity-100 group-focus-within:opacity-100 sm:px-5 sm:text-lg sm:leading-normal"
+      class="pointer-events-none absolute left-1/2 top-full z-50 mt-2 w-max max-w-[min(34rem,calc(100vw-2rem))] -translate-x-1/2 rounded-lg bg-gray-900 px-4 py-3 text-center text-base leading-relaxed text-white shadow-xl ring-1 ring-white/10 opacity-0 transition-opacity duration-75 ease-out group-hover:opacity-100 group-focus-within:opacity-100 sm:px-5 sm:text-lg sm:leading-normal max-[639px]:fixed max-[639px]:top-1/2 max-[639px]:mt-0 max-[639px]:w-[80vw] max-[639px]:max-w-[80vw] max-[639px]:-translate-y-1/2"
     >
       {{ content }}
     </span>
